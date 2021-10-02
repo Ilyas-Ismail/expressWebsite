a071f83a70c0cde5d88bab79de3183bc01a0f295
Ilyas-Ismail <ilyasismaiil@hotmail.com>
2021-10-02 16:02:15 -0400

    Second Website Version
    Major update. Added multiple new pages with content. Added new modules. Added new media files. Added new EJS files.

--- app.js
+++ app.js
@@ -1,24 +1,35 @@
+// File name: app.js
+// Student Name: Muhammad Ilyas "Staz" Sameer Ismail
+// Student ID: 301168447
+// Date: 10/2/2021 (October 2nd, 2021)
+
+// importing the modules
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// associating varibles to route the other js files
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const { builtinModules } = require('module');
 
 var app = express();
 
-// view engine setup
+// view engine setup, configuring the express application
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'node_modules')));
+app.use(express.static(path.join(__dirname, 'Scripts')));
 
+// routing to other page based on link
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
